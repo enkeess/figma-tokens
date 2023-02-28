7307af68649caac46efeb9905206527733556c02
Akhremenko Grigorii <gaahremenko@sbercloud.ru>
2023-02-28 17:51:01 +0300

    feat: mergePackageJson prepack script has been added

---
+++ scripts/mergePackageJson.ts
@@ -0,0 +1,17 @@
+import { writeFileSync } from 'fs';
+import { resolve } from 'path';
+
+(async function main() {
+  const originalPackageJson = await import(resolve(__dirname, '../package.json'));
+  let patchedPackageJson;
+
+  try {
+    patchedPackageJson = await import(resolve(__dirname, '../patch.package.json'));
+  } catch (err) {
+    console.warn('patch file was not found');
+    process.exit(0);
+  }
+
+  const merged = Object.assign({}, originalPackageJson.default, patchedPackageJson.default);
+  writeFileSync(resolve(__dirname, '../package.json'), JSON.stringify(merged, null, 2));
+})();
