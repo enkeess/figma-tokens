db3b3dcb6eb244e48e695d54f6cfad612bb390ba
Nikita Ershov <niershov@cloud.ru>
2025-01-31 09:17:04 +0300

    chore(FF-6137): upd release.config.js

--- release.config.js
+++ release.config.js
@@ -1,1 +1,4 @@
-module.exports = require('@cloud-ru/ft-config-semantic-release').defaultReleaseConfig({ shouldPublishPackage: true });
+module.exports = {
+  repositoryUrl: process.env.CI_GIT_REPOSITORY_URL || undefined,
+  ...require('@cloud-ru/ft-config-semantic-release').defaultReleaseConfig({ shouldPublishPackage: true }),
+};
